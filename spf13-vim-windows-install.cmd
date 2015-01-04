5fcf66095386641dc706aa572c0b76565ad360f1
gspark <zgchen_chibi@163.com>
2015-01-04 15:29:36 +0800

    * set HOME

--- spf13-vim-windows-install.cmd
+++ spf13-vim-windows-install.cmd
@@ -12,16 +12,14 @@ REM    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 REM    See the License for the specific language governing permissions and
 REM    limitations under the License.
 
+set HOME=e:\opt\vim\.home
 
-@if not exist "%HOME%" @set HOME=%HOMEDRIVE%%HOMEPATH%
-@if not exist "%HOME%" @set HOME=%USERPROFILE%
-
-@set APP_DIR=%HOME%\.spf13-vim-3
+@set APP_DIR=%HOME%\gs-vim
 IF NOT EXIST "%APP_DIR%" (
-  call git clone --recursive -b 3.0 https://github.com/spf13/spf13-vim.git "%APP_DIR%"
+  call git clone --recursive https://github.com/gspark/gs-vim.git "%APP_DIR%"
 ) ELSE (
 	@set ORIGINAL_DIR=%CD%
-    echo updating spf13-vim
+    echo updating gs-vim
     chdir /d "%APP_DIR%" 
 	call git pull
     chdir /d "%ORIGINAL_DIR%"
@@ -29,24 +27,12 @@ IF NOT EXIST "%APP_DIR%" (
 )
 
 call mklink "%HOME%\.vimrc" "%APP_DIR%\.vimrc"
-call mklink "%HOME%\_vimrc" "%APP_DIR%\.vimrc"
 call mklink "%HOME%\.vimrc.fork" "%APP_DIR%\.vimrc.fork"
 call mklink "%HOME%\.vimrc.bundles" "%APP_DIR%\.vimrc.bundles"
 call mklink "%HOME%\.vimrc.bundles.fork" "%APP_DIR%\.vimrc.bundles.fork"
 call mklink "%HOME%\.vimrc.before" "%APP_DIR%\.vimrc.before"
 call mklink "%HOME%\.vimrc.before.fork" "%APP_DIR%\.vimrc.before.fork"
-call mklink /J "%HOME%\.vim" "%APP_DIR%\.vim"
+call mklink "%HOME%\.vimrc.bundles.default" "%APP_DIR%\.vimrc.bundles.default"
 
-IF NOT EXIST "%APP_DIR%\.vim\bundle" (
-	call mkdir "%APP_DIR%\.vim\bundle"
-)
 
-IF NOT EXIST "%HOME%/.vim/bundle/vundle" (
-	call git clone https://github.com/gmarik/vundle.git "%HOME%/.vim/bundle/vundle"
-) ELSE (
-  call cd "%HOME%/.vim/bundle/vundle"
-  call git pull
-  call cd %HOME%
-)
-
-call vim -u "%APP_DIR%/.vimrc.bundles" +BundleInstall! +BundleClean +qall
+call vim vim +NeoBundleInstall! +NeoBundleClean +q
